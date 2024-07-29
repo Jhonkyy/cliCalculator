ad7747c74ce60827440e47adb954ec1c6766bcaf
Daniel Duque <danielduquegomez@gmail.com>
2024-07-29 13:34:01 -0500

    create subtrac,multiply and divide functions

--- app/model.py
+++ app/model.py
@@ -3,3 +3,14 @@ class Calculator:
 
     def add(self, num_1: float, num_2: float) -> float:
         return num_1 + num_2
+
+    def subtrac(self, num_1: float, num_2: float) -> float:
+        return num_1 - num_2
+
+    def multiply(self, num_1: float, num_2: float) -> float:
+        return num_1 * num_2
+
+    def divide(self, num_1: float, num_2: float) -> float | str:
+        if num_2 ==0:
+            return "cannot divide by zero (0)"
+        return num_1 / num_2
